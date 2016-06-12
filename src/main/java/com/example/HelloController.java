89163378b6ced57b238f9d04b5888f682a89aba3
miyaketatsuya <miyaketatsuya@192.168.22.137>
2016-06-12 17:03:00 +0900

    first

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.Data;
+ 
+@Controller
+public class HelloController {
+ 
+    @RequestMapping("/")
+    public String index(Model model) {
+        ArrayList<ViewData> list = new ArrayList<ViewData>();
+        for (int i = 0; i < 5; i++) {
+            ViewData data = new ViewData();
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("メッセージ");
+            buffer.append(i);
+            data.setNo(i + 1);
+            data.setMessage(buffer.toString());
+            list.add(data);
+        }
+ 
+        model.addAttribute("msg", list);
+        return "index";
+    }
+}
+ 
+@Data
+class ViewData {
+    private int no;
+    private String message;
+}
